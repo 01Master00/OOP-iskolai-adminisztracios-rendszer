e7161038592962b732f70c00cc2636a9cebe4aa8
01Master00 <berkibalintka@gmail.com>
2025-02-11 08:10:01 +0100

    Add project files.

---
+++ OOP iskolai adminisztracios rendszer BerkiB/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace OOP_iskolai_adminisztracios_rendszer_BerkiB
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
